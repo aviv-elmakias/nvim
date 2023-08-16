635257bfdff80464fefbd3d613224559af0c1056
aviv-elmakias <aviv.elmakias@grip.security>
2023-08-16 21:43:37 +0300

    start

---
+++ lua/custom/plugins/diagnostics.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'weilbith/nvim-code-action-menu',
+  },
+  {
+    'folke/trouble.nvim',
+  },
+  {
+    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+    enabled = false,
+    config = function()
+      local lsp_lines = require 'lsp_lines'
+      vim.diagnostic.config { virtual_text = false }
+      lsp_lines.setup()
+    end,
+  },
+}
