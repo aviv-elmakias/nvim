3547b28f26d3adf3060a75825fba680dfa474aaf
aviv-elmakias <aviv.elmakias@grip.security>
2023-09-10 09:53:30 +0300

    adding autocmd to return to latest window when possible

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -71,3 +71,26 @@ vim.api.nvim_create_autocmd("InsertCharPre", {
         vim.api.nvim_input("<Esc>m'" .. row + 1 .. "gg" .. col + 1 .. "|if<Esc>`'la")
     end,
 })
+
+-- Jump to last accessed window on closing the current one
+WinCloseJmp = function ()
+  -- Exclude floating windows
+  if '' ~= vim.api.nvim_win_get_config(0).relative then return end
+  -- Record the window we jump from (previous) and to (current)
+  if nil == vim.t.winid_rec then
+    vim.t.winid_rec = { prev = vim.fn.win_getid(), current = vim.fn.win_getid() }
+  else
+    vim.t.winid_rec = { prev = vim.t.winid_rec.current, current = vim.fn.win_getid() }
+  end
+
+  -- Loop through all windows to check if the previous one has been closed
+  for winnr=1,vim.fn.winnr('$') do
+    if vim.fn.win_getid(winnr) == vim.t.winid_rec.prev then
+      return        -- Return if previous window is not closed
+    end
+  end
+
+  vim.cmd [[ wincmd p ]]
+end
+
+vim.cmd [[ autocmd VimEnter,WinEnter * lua WinCloseJmp() ]]
