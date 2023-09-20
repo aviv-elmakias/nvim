bf71a2ab1088298d571de27e321545de147ec20f
aviv-elmakias <aviv.elmakias@grip.security>
2023-09-20 11:51:00 +0300

    deleting no name after new buffer opens

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -94,3 +94,14 @@ WinCloseJmp = function ()
 end
 
 vim.cmd [[ autocmd VimEnter,WinEnter * lua WinCloseJmp() ]]
+
+
+-- delete [no name] buffer
+vim.api.nvim_create_autocmd("BufHidden", {
+  desc = "Delete [No Name] buffers",
+  callback = function(event)
+    if event.file == "" and vim.bo[event.buf].buftype == "" and not vim.bo[event.buf].modified then
+      vim.schedule(function() pcall(vim.api.nvim_buf_delete, event.buf, {}) end)
+    end
+  end,
+})
