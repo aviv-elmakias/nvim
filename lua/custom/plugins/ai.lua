9ffd7b9453d9d4efb70a3a99a3240a4b1f707b45
Aviv Elmakias <aviv.elmakias.b@gmail.com>
2023-12-08 21:15:14 +0200

    adding the option to codium for personal use

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -49,4 +49,13 @@ return {
       })
     end,
   },
+  -- {
+  --   'Exafunction/codeium.vim',
+  --   config = function()
+  --     vim.g.codeium_no_map_tab = 1
+  --     vim.keymap.set('i', '<M-l>', function()
+  --       return vim.fn['codeium#Accept']()
+  --     end, { expr = true })
+  --   end,
+  -- },
 }
