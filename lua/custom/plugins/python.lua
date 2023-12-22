9b31b6eaba5bea50bc5f0227fcb28579a7e501cd
Aviv Elmakias <aviv.elmakias.b@gmail.com>
2023-12-22 17:19:29 +0200

    it's not working without telescope for now

--- lua/custom/plugins/python.lua
+++ lua/custom/plugins/python.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    'linux-cultist/venv-selector.nvim',
-    opts = {
-      search = false,
-      parents = 0,
-    },
-    keys = { { '<leader>lv', '<cmd>:VenvSelect<cr>', desc = 'Select VirtualEnv' } },
-  },
+  -- {
+  --   'linux-cultist/venv-selector.nvim',
+  --   opts = {
+  --     search = false,
+  --     parents = 0,
+  --   },
+  --   keys = { { '<leader>lv', '<cmd>:VenvSelect<cr>', desc = 'Select VirtualEnv' } },
+  -- },
   -- {
   --   'avivelmakias1/venv-selector.nvim',
   --   dev = true,
