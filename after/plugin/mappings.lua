63c77c956900fe2eb3e101836f911d055e25a02a
aviv-elmakias <aviv.elmakias@grip.security>
2024-01-17 09:38:37 +0200

    moving back to telescope

--- after/plugin/mappings.lua
+++ after/plugin/mappings.lua
@@ -50,21 +50,21 @@ vim.keymap.set('n', '<leader>to', '<cmd>OverseerToggle<cr>', { desc = 'Overseer 
 
 vim.keymap.set('n', '<leader>zm', '<cmd>ZenMode<cr>', { desc = 'Zen Mode' })
 
--- lua fzf
-vim.keymap.set('n', '<leader>ff', '<cmd>lua require("fzf-lua").files()<cr>', { desc = 'Find Files (root dir)' })
-vim.keymap.set('n', '<leader><leader>', '<cmd>lua require("fzf-lua").files()<cr>', { desc = 'Find Files (root dir)' })
-vim.keymap.set('n', '<leader>fg', '<cmd>lua require("fzf-lua").live_grep()<cr>', { desc = 'Grep (root dir)' })
-vim.keymap.set('n', '<leader>fb', '<cmd>lua require("fzf-lua").buffers()<cr>', { desc = 'Buffers' })
-vim.keymap.set('n', '<leader>fh', '<cmd>lua require("fzf-lua").help_tags()<cr>', { desc = 'Help Pages' })
-vim.keymap.set('n', '<leader>fm', '<cmd>lua require("fzf-lua").marks()<cr>', { desc = 'Jump to Mark' })
-vim.keymap.set('n', '<leader>fr', '<cmd>lua require("fzf-lua").resume()<cr>', { desc = 'Resume' })
-vim.keymap.set('n', '<leader>fo', '<cmd>lua require("fzf-lua").oldfiles()<cr>', { desc = 'Old Files' })
-vim.keymap.set('n', '<leader>fw', '<cmd>lua require("fzf-lua").grep_cword()<cr>', { desc = 'Word (root dir)' })
-vim.keymap.set('n', '<leader>fs', '<cmd>lua require("fzf-lua").grep_curbuf()<cr>', { desc = 'Search current buffer' })
-vim.keymap.set('n', '<leader>fc', '<cmd>lua require("fzf-lua").commands()<cr>', { desc = 'Commands' })
-vim.keymap.set('n', '<leader>fa', '<cmd>lua require("fzf-lua").autocommands()<cr>', { desc = 'Auto Commands' })
-vim.keymap.set('n', '<leader>fk', '<cmd>lua require("fzf-lua").keymaps()<cr>', { desc = 'Key Maps' })
-vim.keymap.set('v', '<leader>fw', '<cmd>lua require("fzf-lua").grep_visual()<cr>', { desc = 'Word (root dir)' })
+-- -- lua fzf
+-- vim.keymap.set('n', '<leader>ff', '<cmd>lua require("fzf-lua").files()<cr>', { desc = 'Find Files (root dir)' })
+-- vim.keymap.set('n', '<leader><leader>', '<cmd>lua require("fzf-lua").files()<cr>', { desc = 'Find Files (root dir)' })
+-- vim.keymap.set('n', '<leader>fg', '<cmd>lua require("fzf-lua").live_grep()<cr>', { desc = 'Grep (root dir)' })
+-- vim.keymap.set('n', '<leader>fb', '<cmd>lua require("fzf-lua").buffers()<cr>', { desc = 'Buffers' })
+-- vim.keymap.set('n', '<leader>fh', '<cmd>lua require("fzf-lua").help_tags()<cr>', { desc = 'Help Pages' })
+-- vim.keymap.set('n', '<leader>fm', '<cmd>lua require("fzf-lua").marks()<cr>', { desc = 'Jump to Mark' })
+-- vim.keymap.set('n', '<leader>fr', '<cmd>lua require("fzf-lua").resume()<cr>', { desc = 'Resume' })
+-- vim.keymap.set('n', '<leader>fo', '<cmd>lua require("fzf-lua").oldfiles()<cr>', { desc = 'Old Files' })
+-- vim.keymap.set('n', '<leader>fw', '<cmd>lua require("fzf-lua").grep_cword()<cr>', { desc = 'Word (root dir)' })
+-- vim.keymap.set('n', '<leader>fs', '<cmd>lua require("fzf-lua").grep_curbuf()<cr>', { desc = 'Search current buffer' })
+-- vim.keymap.set('n', '<leader>fc', '<cmd>lua require("fzf-lua").commands()<cr>', { desc = 'Commands' })
+-- vim.keymap.set('n', '<leader>fa', '<cmd>lua require("fzf-lua").autocommands()<cr>', { desc = 'Auto Commands' })
+-- vim.keymap.set('n', '<leader>fk', '<cmd>lua require("fzf-lua").keymaps()<cr>', { desc = 'Key Maps' })
+-- vim.keymap.set('v', '<leader>fw', '<cmd>lua require("fzf-lua").grep_visual()<cr>', { desc = 'Word (root dir)' })
 
 -- Diagnostics
 vim.keymap.set('n', '<leader>qq', '<cmd>TroubleToggle document_diagnostics<cr>', { desc = 'Trouble Document' })
